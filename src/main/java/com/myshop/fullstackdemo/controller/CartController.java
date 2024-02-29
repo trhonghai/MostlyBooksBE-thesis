7bb1c647379026cf3d47c2176f7aa87bf4d9210c
trhonghai <trhonghaii@gmail.com>
2024-02-29 12:00:21 +0700

    Update 28/2

--- src/main/java/com/myshop/fullstackdemo/controller/CartController.java
+++ src/main/java/com/myshop/fullstackdemo/controller/CartController.java
@@ -9,15 +9,13 @@ import com.myshop.fullstackdemo.model.Cart;
 import com.myshop.fullstackdemo.model.OrderDetail;
 import com.myshop.fullstackdemo.repository.BookRepository;
 import com.myshop.fullstackdemo.repository.CartRepository;
+import com.myshop.fullstackdemo.repository.OrderDetailRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,18 +30,23 @@ public class CartController {
 
     private final BookRepository bookRepository;
 
+    private final OrderDetailRepository  orderDetailRepository;
 
-    @GetMapping("/add-to-cart")
 
+    @GetMapping
+    public ResponseEntity<List<OrderDetail>> getCart(@RequestParam("cart") Long cartId){
+        List<OrderDetail> orderDetails = cartRepository.findById(cartId).orElseThrow().getOrderDetail();
+        return ResponseEntity.ok(orderDetails);
+    }
+
+
+    @PostMapping("/add-to-cart")
     public ResponseEntity<OrderDetail> addToCart(@RequestBody CartItem cartItem){
         Cart cart = cartRepository.findById((long) cartItem.getCartId()).orElseThrow(()->new NotFoundException("cart " +
                 "not found, " +
                 "id="+cartItem.getCartId()));
         Book book = entityManager.getReference(Book.class, cartItem.getBookId());
-        System.out.println(cart);
-        System.out.println(book);
         List<OrderDetail> orderDetails = cart.getOrderDetail();
-
         for (OrderDetail orderDetail : orderDetails) {
             if (orderDetail.getBook().getId().equals((long) cartItem.getBookId())) {
                 orderDetail.setQuantity(orderDetail.getQuantity() + 1);
@@ -51,17 +54,25 @@ public class CartController {
                 return ResponseEntity.ok(orderDetail);
             }
         }
-
         OrderDetail newOrderDetail = new OrderDetail();
         newOrderDetail.setBook(book);
         newOrderDetail.setPrice(cartItem.getPrice());
         newOrderDetail.setQuantity(cartItem.getQuantity());
         newOrderDetail.setCart(cart);
+        orderDetailRepository.save(newOrderDetail);
         orderDetails.add(newOrderDetail);
-
         cartRepository.save(cart);
         return ResponseEntity.ok(newOrderDetail);
-
     }
+
+
+
+
+
+//    @PutMapping("/update-quantity")
+//    public ResponseEntity<List<OrderDetail>> updateItemQuantity(@RequestParam("cart") String cartId)
+
+
+
 
 }
